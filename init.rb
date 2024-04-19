ef2a0cd4666ee95b3e763031d1afc011d7ac025f
Daniel Bengl <daniel.bengl@renuo.ch>
2024-04-19 13:29:29 +0200

    Add tutorial files
    Fix something

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
+# require_dependency File.expand_path("lib/polls_hook_listener", __dir__)
+
 Redmine::Plugin.register :redmine_3cx_plugin do
   name "3CX Plugin Redmine"
   author "Daniel Bengl"
@@ -5,4 +7,19 @@ Redmine::Plugin.register :redmine_3cx_plugin do
   version "0.0.1"
   url "https://github.com/renuo/redmine-3cx-plugin"
   author_url "http://example.com/about"
+  menu :application_menu, :polls, {controller: "polls", action: "index"}, caption: "Polls"
+  permission :polls, {polls: [:index, :vote]}, public: true
+  menu :project_menu, :polls, {controller: "polls", action: "index"}, caption: "Polls", after: :activity, param: :project_id
+  delete_menu_item :top_menu, :my_page
+  delete_menu_item :top_menu, :help
+  delete_menu_item :project_menu, :overview
+  delete_menu_item :project_menu, :activity
+  delete_menu_item :project_menu, :news
+  permission :view_polls, polls: :index
+  permission :vote_polls, polls: :vote
+  project_module :polls do
+    permission :view_polls, polls: :index
+    permission :vote_polls, polls: :vote
+  end
+  settings default: {"empty" => true}, partial: "settings/poll_settings"
 end
