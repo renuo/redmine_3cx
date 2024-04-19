da7dbfcf2f75d59704495109d63f6979b59b45c9
Daniel Bengl <daniel.bengl@renuo.ch>
2024-04-19 11:02:07 +0200

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_3cx_plugin do
+  name "3CX Plugin Redmine"
+  author "Daniel Bengl"
+  description "A simple 3CX integration plugin."
+  version "0.0.1"
+  url "https://github.com/renuo/redmine-3cx-plugin"
+  author_url "http://example.com/about"
+end
