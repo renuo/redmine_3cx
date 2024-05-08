735e8ac8b5e74a66c75df014d26defedc384402a
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-08 19:22:59 +0200

    Refactor benchmark

--- test/functional/polls_controller_test.rb
+++ test/functional/polls_controller_test.rb
@@ -10,16 +10,17 @@ class PollsControllerTest < ActionController::TestCase
   end
 
   def test_index_performance
-    test_results = []
-    20.times do
-      benchmark = Benchmark.ms do
-        get :index, params: {project_id: 1}
-        assert_response :success
-      end
+    test_results = 20.times.map { benchmark_index_call }
+    error_message = "Rendering the index page in the 95th percentile took more than 100ms"
+    assert test_results.sort[-2] < 100, error_message
+  end
 
-      test_results << benchmark
+  private
+
+  def benchmark_index_call
+    Benchmark.ms do
+      get :index, params: {project_id: 1}
+      assert_response :success
     end
-
-    assert test_results.sort[-2] < 100, "Rendering the index page in the 95th percentile took more than 100ms"
   end
 end
