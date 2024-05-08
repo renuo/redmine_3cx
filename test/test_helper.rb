6769299109d2634fd379efc6d16032c931a73102
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-08 15:58:42 +0200

    Remove branch coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,10 @@
 # Load the Redmine helper
 require "simplecov"
 SimpleCov.start do
-  enable_coverage :branch
   add_filter "/test/"
   add_filter do |source_file|
     source_file.filename !~ %r{/plugins/redmine_3cx/}
   end
 end
-SimpleCov.minimum_coverage line: 100, branch: 100
+SimpleCov.minimum_coverage 100
 require File.expand_path(File.dirname(__FILE__) + "/../../../test/test_helper")
