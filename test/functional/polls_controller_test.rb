acb7afd63e581017b4dbad5f0d959022a37269d8
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-08 19:18:55 +0200

    Move performance test to functional tests

--- test/functional/polls_controller_test.rb
+++ test/functional/polls_controller_test.rb
@@ -8,4 +8,18 @@ class PollsControllerTest < ActionController::TestCase
 
     assert_response :success
   end
+
+  def test_index_performance
+    test_results = []
+    20.times do
+      benchmark = Benchmark.ms do
+        get :index, params: {project_id: 1}
+        assert_response :success
+      end
+
+      test_results << benchmark
+    end
+
+    assert test_results.sort[-2] < 100, "Rendering the index page in the 95th percentile took more than 100ms"
+  end
 end
