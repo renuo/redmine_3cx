f07e8219992db092ecbce847926af8848db1da5a
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-10 08:59:47 +0200

    Add test

--- test/functional/crm_template_controller_test.rb
+++ test/functional/crm_template_controller_test.rb
@@ -1,8 +1,19 @@
-require_relative '../test_helper'
+require_relative "../test_helper"
 
 class CrmTemplateControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_template_response
+    get :index, format: :xml
+    assert_includes response.content_type, "application/xml"
+    assert_response :success
+  end
+
+  def test_template_contents
+    get :index, format: :xml
+    assert_includes response.body, "<Crm"
+    assert_includes response.body, '<Authentication Type="Basic">'
+    assert_includes response.body, "<Scenarios"
+    assert_includes response.body, '<Scenario Type="REST">'
+    assert_includes response.body, '<Scenario Id="CreateContactRecord" Type="REST">'
   end
 end
+
