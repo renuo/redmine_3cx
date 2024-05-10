79f0c48f7bab706bca2147b94b998832258b3f2d
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-10 10:55:14 +0200

    Attempt fix compatiblity

--- app/controllers/crm_api_controller.rb
+++ app/controllers/crm_api_controller.rb
@@ -1,6 +1,6 @@
 class CrmApiController < ApplicationController
   def show
-    @contact = Contact.find_by(params[:phone])
+    @contact = Contact.find_by(phone: params[:phone])
     render json: {contact: {
       firstname: @contact.first_name,
       lastname: @contact.last_name,
