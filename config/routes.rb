b712c4df832cc5af4278ead700b265895ef53e38
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-10 12:36:25 +0200

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-get "3cx_contacts", to: "crm_api#show", defaults: {format: "json"}
+get "3cx/contacts", to: "crm_api#show", defaults: {format: "json"}
