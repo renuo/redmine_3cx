8131ddf4fb08ca8683aa5e02c9db808b2ca123be
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-10 10:51:05 +0200

    Update api

--- test/functional/crm_api_controller_test.rb
+++ test/functional/crm_api_controller_test.rb
@@ -54,5 +54,24 @@ class CrmApiControllerTest < ActionController::TestCase
   def get_contact_assert_success
     get_contact(@contact.phone)
     assert_response :success
+  def test_truth
+    assert true
+  def setup
+    @contact = create(:contact)
+  end
+
+  def test_show
+    get :show, format: :json
+    assert_response :success
+    assert_equal "application/json", response.content_type
+
+    assert_equal json_response, {
+      "contact" => {
+        "id" => @contact.id,
+        "firstname" => "John",
+        "lastname" => "Doe",
+        "phone" => "1234567890"
+      }
+    }
   end
 end
