aa035a42b73d77d67ef48239fe46e9f3ae355eeb
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-10 09:45:47 +0200

    Generate controller

---
+++ app/controllers/crm_api_controller.rb
@@ -0,0 +1,2 @@
+class CrmApiController < ApplicationController
+end
---
+++ test/functional/crm_api_controller_test.rb
@@ -0,0 +1,7 @@
+require_relative "../test_helper"
+
+class CrmApiControllerTest < ActionController::TestCase
+  def test_truth
+    assert true
+  end
+end
