65fb208a50883867b19edf7b01ce61fdcc74ed7f
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-10 10:57:35 +0200

    Implement not found case

--- app/controllers/crm_api_controller.rb
+++ app/controllers/crm_api_controller.rb
@@ -1,6 +1,11 @@
 class CrmApiController < ApplicationController
   def show
     @contact = Contact.find_by(phone: params[:phone])
+
+    if @contact.nil?
+      return render json: {error: "Not found"}, status: :not_found
+    end
+
     render json: {contact: {
       firstname: @contact.first_name,
       lastname: @contact.last_name,
