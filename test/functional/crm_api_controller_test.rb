3b3b1b7ea94e69e60632f2d6b6c9b0d186bfde21
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-10 11:04:35 +0200

    Add performance test

--- test/functional/crm_api_controller_test.rb
+++ test/functional/crm_api_controller_test.rb
@@ -7,7 +7,7 @@ class CrmApiControllerTest < ActionController::TestCase
   end
 
   def test_show
-    get :show, params: {phone: "1234567890"}, format: :json
+    get_show_page
     assert_response :success
     assert_includes "application/json; charset=utf-8", response.content_type
 
@@ -24,5 +24,29 @@ class CrmApiControllerTest < ActionController::TestCase
     get :show, params: {phone: "not-found"}, format: :json
     assert_response :not_found
     assert_equal response.body, {error: "Not found"}.to_json
+  end
+
+  def test_performance
+    percentile = 95
+    n = 100 / (100 - percentile)
+
+    max_response_time_ms = 100
+
+    results = n.times.map { benchmark_show_call }
+    error_message = "Rendering the show page in the #{percentile}th percentile took more than #{max_response_time_ms}ms"
+    assert results.sort[-2] < max_response_time_ms, error_message
+  end
+
+  private
+
+  def get_show_page
+    get :show, params: {phone: @contact.phone}, format: :json
+  end
+
+  def benchmark_show_call
+    Benchmark.ms do
+      get_show_page
+      assert_response :success
+    end
   end
 end
