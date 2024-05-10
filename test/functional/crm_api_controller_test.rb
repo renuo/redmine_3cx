52d6bb9a3730160014fe836076e43c03af3bc02e
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-10 09:52:26 +0200

    Implement controller test

--- test/functional/crm_api_controller_test.rb
+++ test/functional/crm_api_controller_test.rb
@@ -1,7 +1,22 @@
 require_relative "../test_helper"
 
 class CrmApiControllerTest < ActionController::TestCase
-  def test_truth
-    assert true
+  def setup
+    @contact = create(:contact)
+  end
+
+  def test_show
+    get :show, format: :json
+    assert_response :success
+    assert_equal "application/json", response.content_type
+
+    assert_equal json_response, {
+      "contact" => {
+        "id" => @contact.id,
+        "firstname" => "John",
+        "lastname" => "Doe",
+        "phone" => "1234567890"
+      }
+    }
   end
 end
