ef94d4eff0a3c5164eb6e727703be3a3dd2af39b
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-13 14:45:18 +0200

    Refactor extract method

--- app/controllers/crm_api_controller.rb
+++ app/controllers/crm_api_controller.rb
@@ -1,16 +1,22 @@
 class CrmApiController < ApplicationController
+  before_action :find_user, only: [:show]
+
   def show
-    @contact = Contact.find_by(phone: params[:phone])
-
-    if @contact.nil?
-      return render json: {error: "Not found"}, status: :not_found
-    end
-
     render json: {contact: {
       firstname: @contact.first_name,
       lastname: @contact.last_name,
       company: @contact.company,
       phone: @contact.phone
     }}
+  end
+
+  private
+
+  def find_user
+    @contact = Contact.find_by(phone: params[:phone])
+
+    if @contact.nil?
+      render json: {error: "Not found"}, status: :not_found
+    end
   end
 end
