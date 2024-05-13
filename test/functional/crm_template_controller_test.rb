6c7d4fef90573420643ba054d0eef0b67e0d9e1d
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-13 16:53:22 +0200

    Add nokogiri template validity test

--- test/functional/crm_template_controller_test.rb
+++ test/functional/crm_template_controller_test.rb
@@ -1,4 +1,5 @@
 require_relative "../test_helper"
+require "nokogiri"
 
 class CrmTemplateControllerTest < ActionController::TestCase
   def setup
@@ -9,6 +10,14 @@ class CrmTemplateControllerTest < ActionController::TestCase
     assert_includes @response.content_type, "application/xml"
     assert_template "crm_template/show"
     assert_response :success
+  end
+
+  def test_template_valid_xml
+    doc = Nokogiri::XML(@response.body)
+    assert_empty doc.errors
+    # doc errors are empty even if there are unclosed tags
+    # these can be checked with doc.text
+    assert_empty doc.text.delete("\n").strip
   end
 
   def test_template_contents
