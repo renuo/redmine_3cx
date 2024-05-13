aad65b9d4567475992d4fb3e21da48658a3d2db4
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-13 10:28:31 +0200

    Make XML template downloadable

--- app/controllers/crm_template_controller.rb
+++ app/controllers/crm_template_controller.rb
@@ -1,4 +1,6 @@
 class CrmTemplateController < ApplicationController
   def show
+    stream = render_to_string(template: "template/show")
+    send_data(stream, type: "text/xml", filename: "template.xml")
   end
 end
