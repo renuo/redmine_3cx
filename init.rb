d1054d5c3b92d00848e5f8bd98a8d909f230425e
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-13 15:54:57 +0200

    Update plugin init file

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_3cx do
   name "Redmine 3CX Plugin"
   author "Daniel Bengl"
   description "A simple 3CX integration plugin."
-  version "0.0.1"
+  version "0.2.0"
   url "https://github.com/renuo/redmine_3cx"
-  author_url "http://example.com/about"
+  author_url "https://github.com/CuddlyBunion341"
 end
