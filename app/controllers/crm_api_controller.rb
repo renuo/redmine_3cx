03e4fa17dd0a60225394624e7871754645bdf70e
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-14 18:46:55 +0200

    Add join

--- app/controllers/crm_api_controller.rb
+++ app/controllers/crm_api_controller.rb
@@ -17,7 +17,7 @@ class CrmApiController < ApplicationController
   def find_contacts
     phone_number = ContactSerializer.map_phone_number(params[:phone])
 
-    @contacts = Contact.all.filter do |contact|
+    @contacts = Contact.joins(:projects).filter do |contact|
       contact.phones.map { |phone| ContactSerializer.map_phone_number(phone) }.include?(phone_number)
     end.filter do |contact|
       User.current.allowed_to?(:use_api, contact.project)
