fde9f9fcd7a0c0173ff1de48bc3fd9e24c4ee328
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-14 11:15:19 +0200

    Update redmine 3cx version

--- init.rb
+++ init.rb
@@ -1,10 +1,8 @@
-REDMINE_3CX_VERSION = "0.2.0"
-
 Redmine::Plugin.register :redmine_3cx do
   name "Redmine 3CX Plugin"
   author "Daniel Bengl"
   description "A simple 3CX integration plugin."
-  version REDMINE_3CX_VERSION
+  version "0.3.0"
   url "https://github.com/renuo/redmine_3cx"
   author_url "https://github.com/CuddlyBunion341"
 end
