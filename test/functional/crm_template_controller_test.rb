f49ece731c20dd489a3c2749c6108d69831df583
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-29 17:07:57 +0200

    Refactor crm template controller, add auth

--- test/functional/crm_template_controller_test.rb
+++ test/functional/crm_template_controller_test.rb
@@ -2,26 +2,36 @@ require_relative "../test_helper"
 require "nokogiri"
 
 class CrmTemplateControllerTest < ActionController::TestCase
+  fixtures :users
+
   def test_template_response
-    get :template, format: :xml
-    assert_includes @response.content_type, "application/xml"
-    assert_template "crm_template/template"
+    @request.session[:user_id] = 1
+    get :template
     assert_response :success
+    assert_includes @response.content_type, "application/xml"
+    assert_template "crm_template/template.xml"
   end
 
   def test_template_valid_xml
+    @request.session[:user_id] = 1
     get :template, format: :xml
     doc = Nokogiri::XML(@response.body)
     assert_empty doc.errors
   end
 
   def test_template_contents
-    get :template, format: :xml
+    @request.session[:user_id] = 1
+    get :template
     options = Nokogiri::XML::ParseOptions.new(Nokogiri::XML::ParseOptions::NOBLANKS)
 
     doc = Nokogiri::XML(@response.body, nil, nil, options)
     assert_equal "Crm", doc.root.name
     assert_not_nil doc.at_xpath("//Authentication")
     assert_not_nil doc.at_xpath("//Scenarios")
+  end
+
+  def test_unauthorized
+    get :template, format: :xml
+    assert_response :forbidden
   end
 end
