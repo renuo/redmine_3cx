650970510b836fb10e588d1dd9d3e5cbdd6f6923
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-29 09:05:16 +0200

    Refactor template controller test

--- test/functional/crm_template_controller_test.rb
+++ test/functional/crm_template_controller_test.rb
@@ -2,28 +2,26 @@ require_relative "../test_helper"
 require "nokogiri"
 
 class CrmTemplateControllerTest < ActionController::TestCase
-  def setup
-    get :template, format: :xml
-  end
-
   def test_template_response
+    get :template, format: :xml
     assert_includes @response.content_type, "application/xml"
     assert_template "crm_template/template"
     assert_response :success
   end
 
   def test_template_valid_xml
+    get :template, format: :xml
     doc = Nokogiri::XML(@response.body)
     assert_empty doc.errors
   end
 
   def test_template_contents
-    assert_includes @response.body, "<Crm"
-    assert_includes @response.body, "<Scenarios"
-    assert_includes @response.body, "<Scenarios"
-    assert_includes @response.body, "contacts.id"
-    assert_includes @response.body, "contacts.firstname"
-    assert_includes @response.body, "contacts.lastname"
-    assert_includes @response.body, "contacts.company"
+    get :template, format: :xml
+    options = Nokogiri::XML::ParseOptions.new(Nokogiri::XML::ParseOptions::NOBLANKS)
+
+    doc = Nokogiri::XML(@response.body, nil, nil, options)
+    assert_equal "Crm", doc.root.name
+    assert_not_nil doc.at_xpath("//Authentication")
+    assert_not_nil doc.at_xpath("//Scenarios")
   end
 end
