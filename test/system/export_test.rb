2dd257ff412f45ca8f5aa4f6f2d0381d005ca27e
Daniel Bengl <daniel.bengl@renuo.ch>
2025-03-03 09:53:02 +0100

    Revert "Attempt fix setup"
    This reverts commit 2db090d8a115e32723d30dcc628c5435d6f47489.

--- test/system/export_test.rb
+++ test/system/export_test.rb
@@ -6,14 +6,10 @@ class ExportTest < ActiveSupport::TestCase
   fixtures :roles, :users
 
   def setup
-    require "rake"
-    Rake.application.load_rakefile
-
     create(:contact, first_name: "John", last_name: "Doe", company: "Example AG", phone: "0781234567")
     create(:contact, first_name: "Other", last_name: "Person", company: "Example AG", phone: "+41 78 123 45 67, 078 123 45 67, 078 123 45 67")
     create(:contact, first_name: "Another", last_name: "Person", company: "Example AG", phone: "+41 (0) 78 123 45 67")
     create(:contact, first_name: "Yet", last_name: "Another", company: "Example AG", phone: "+41 78 123 45 67")
-
     Rake::Task["redmine_3cx:export"].invoke
     @csv = CSV.read("tmp/contacts.csv")
   end
