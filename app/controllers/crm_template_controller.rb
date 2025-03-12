9909ed81ef23575a562a5a680a245f03eb90c741
Daniel Bengl <53896675+CuddlyBunion341@users.noreply.github.com>
2025-03-12 09:40:23 +0100

    Update to Redmine6 (#14)
    * Add Redmine6 to semaphore workflow
    
    * Attempt fix redmine checkout
    
    * Clear cache
    
    * Attempt fix REDMINE4
    
    * Attempt fix setup
    
    Attempt fix deps
    
    Attempt fix rake
    
    Attempt fix coverage
    
    Work please
    
    Revert "Clear cache"
    
    This reverts commit 90aa08ed7d1d174fc0cd37cd20879c0866e6cc18.
    
    Attempt fix
    
    * Revert "Attempt fix setup"
    
    This reverts commit 2db090d8a115e32723d30dcc628c5435d6f47489.
    
    * Improve coverage
    
    * Attempt fix spec
    
    * Attempt fix Redmine4
    
    * Attempt fix nokogiri
    
    * Attempt fix nokogiri
    
    * Attempt fix redmine setup?
    
    * Drop Redmine 4 requirement
    
    * Fix template tests for Redmine6
    
    * Fix settings test
    
    * Attempt fix contacts
    
    * Apply suggestions from code review
    
    * Attempt fix cache???
    
    * Attempt fix CI
    
    * Update workflow

--- app/controllers/crm_template_controller.rb
+++ app/controllers/crm_template_controller.rb
@@ -2,6 +2,6 @@ class CrmTemplateController < ApplicationController
   before_action :require_admin
 
   def template
-    render "crm_template/template.xml", content_type: "application/xml"
+    render :template, formats: [:xml], layout: false, content_type: "application/xml"
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ SimpleCov.root File.expand_path(File.dirname(__FILE__) + "/../")
 SimpleCov.minimum_coverage 100
 SimpleCov.start do
   add_filter "/test/system/settings_test.rb" if ENV["CI"] # Remove once capybara is setup for CI
+  add_filter "/lib/tasks/run.rake"
+  add_filter "/lib/tasks/test.rake"
 end
 require "rails-controller-testing"
 Rails::Controller::Testing.install
