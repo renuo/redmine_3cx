8bc04d3143ee939f01bf51e7a76bdf3f586017b0
Daniel Bengl <daniel.bengl@renuo.ch>
2026-01-28 16:19:52 +0100

    Revert "Attmept refactor csv generation"
    This reverts commit 1a3305d4eb399e7ee1be8dc41873b9ce3c29fcb0.

--- app/models/contact_serializer.rb
+++ app/models/contact_serializer.rb
@@ -1,15 +1,5 @@
 class ContactSerializer
-  PHONE_NUMBER_KEYS = [:phone_business, :phone_business2, :phone_home, :phone_home2, :phone_mobile, :phone_other]
-
-  MAPPING = {
-    0 => :phone_business,
-    1 => :phone_business2,
-    2 => :phone_home,
-    # 0 => :phone_home2,
-    3 => :phone_mobile,
-    4 => :phone_mobile2,
-    5 => :phone_other,
-  }
+  PHONE_NUMBER_KEYS = [:phone_business, :phone_business2, :phone_home, :phone_home2, :phone_mobile, :phone_mobile2, :phone_other]
 
   class << self
     def call(contact)
@@ -24,17 +14,6 @@ class ContactSerializer
       # We map them in the order they are stored, as defined by the PHONE_NUMBER_KEYS constant.
       phone_numbers = map_phone_numbers_to_keys(contact.phones)
       contact_hash.merge(phone_numbers.compact)
-    end
-
-    def contact_number(contact, number_kind)
-      reverse_mapping =  ContactSerializer::MAPPING.map { |k,v| [v,k] }.to_h
-phone_number =       contact.phones[reverse_mapping.fetch(number_kind) do
-        raise "'#{number_kind}' is not included in [ #{reverse_mapping.keys.join(' ')} ]"
-      end]
-
-      return nil if phone_number.nil?
-
-      normalize_phone_number(phone_number)
     end
 
     def map_phone_numbers_to_keys(phone_numbers)
